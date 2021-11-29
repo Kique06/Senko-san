1b69da44ecacb4a7f279bbef8f47deb7c4389cdb
Kique06 <88282394+Kique06@users.noreply.github.com>
2021-11-29 10:44:52 -0300

    Update bugreport.js

--- commands/bugreport.js
+++ commands/bugreport.js
@@ -1,7 +1,6 @@
 const Discord = require('discord.js')
 const client = new Discord.Client();
 const myID = 'YourIdHere' 
-//var lockedList = ["680893803590189096"]; 
 
 
 exports.run = async (client, message, args) => {
