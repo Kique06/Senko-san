89aa5d7fc710c97f7f4124ee1cb650cf1d32a40c
Kique06 <88282394+Kique06@users.noreply.github.com>
2022-03-13 19:27:00 -0300

    Update invite.js

--- commands/invite.js
+++ commands/invite.js
@@ -1,16 +1,13 @@
 const Discord = require('discord.js')
 
 exports.run = async (client, message, args) => {
-
-if (user = message.mentions.users.first() || client.users.cache.get(args[0])) return;
    
-
-let avatar = message.author.displayAvatarURL({format: "png"});
-const embed = new Discord.MessageEmbed()
-    .setTitle('Want to add me to your server?')
-    .setColor('5a5a5a')
-    .setDescription('Click here to add me! --> [link](<https://discord.com/api/oauth2/authorize?client_id=818887799104471100&permissions=137475935824&scope=bot>)')
-    .setFooter('Ty')
-    .setAuthor(message.author.tag, avatar);
-await message.channel.send(embed);
+   let avatar = message.author.displayAvatarURL({format: "png"});
+   const embed = new Discord.MessageEmbed()
+       .setTitle('Want to add me to your server?')
+       .setColor('5a5a5a')
+       .setDescription('Click here to add me! --> [link](<Your invite here>)')
+       .setFooter('Ty')
+       .setAuthor(message.author.tag, avatar);
+   await message.channel.send(embed);
 }
